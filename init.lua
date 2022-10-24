1654684841772828f7f0004316b3290c5f9cbd55
Romedius <rom3dius@yahoo.com>
2022-10-24 02:17:05 +0200

    added split management

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ return require('packer').startup(function(use)
 	"windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   }
+  use('mrjones2014/smart-splits.nvim')
   use({
 	"Pocco81/auto-save.nvim",
 	config = function()
@@ -87,8 +88,57 @@ return require('packer').startup(function(use)
   if packer_bootstrap then
     require('packer').sync()
   end
+  -- SETUP COC TAB COMPLETION and COC misc
+  vim.cmd([[function! CheckBackspace() abort
+    let col = col('.') - 1
+    return !col || getline('.')[col - 1]  =~# '\s'
+  endfunction
+
+  inoremap <silent><expr> <Tab>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()]])
+  vim.keymap.set('', '<leader>d', ':<C-U>CocDiagnostics<CR>')
   -- SETUP MAPLEADER
   vim.g.mapleader = ","
+  -- SMART SPLIT SETUP
+  -- resizing splits
+  -- amount defaults to 3 if not specified
+  -- use absolute values, no + or -
+  require('smart-splits').resize_up(amount)
+  require('smart-splits').resize_down(amount)
+  require('smart-splits').resize_left(amount)
+  require('smart-splits').resize_right(amount)
+  -- moving between splits
+  -- pass same_row as a boolean to override the default
+  -- for the move_cursor_same_row config option.
+  -- See Configuration.
+  require('smart-splits').move_cursor_up()
+  require('smart-splits').move_cursor_down()
+  require('smart-splits').move_cursor_left(same_row)
+  require('smart-splits').move_cursor_right(same_row)
+  -- persistent resize mode
+  -- temporarily remap your configured resize keys to
+  -- smart resize left, down, up, and right, respectively,
+  -- press <ESC> to stop resize mode (unless you've set a different key in config)
+  -- resize keys also accept a range, e.e. pressing `5j` will resize down 5 times the default_amount
+  -- require('smart-splits').start_resize_mode()
+  
+  -- recommended mappings
+  -- resizing splits
+  vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+  vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+  vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+  vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+  -- moving between splits
+  vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+  vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+  vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+  vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right) 
+  -- do a split
+  vim.keymap.set('','<leader>h', ':split<CR>')
+  vim.keymap.set('','<leader>v', ':vsplit<CR>')
+ 
   -- NIGHTFOX SETUP
   vim.cmd("colorscheme nightfox")
   -- LEADERF SETUP
@@ -96,12 +146,16 @@ return require('packer').startup(function(use)
   vim.keymap.set('', '<leader>ff', ':<C-U>Leaderf file<CR>')
   vim.keymap.set('', '<leader>fg', ':<C-U>Leaderf rg<CR>')
   vim.keymap.set('', '<leader>fh', ':<C-U>Leaderf help<CR>')
+  -- SETUP COPILOT
+  vim.g.copilot_no_tab_map = true
+  vim.api.nvim_set_keymap("i", "<C-E>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   -- SETUP FUGITIVE
   vim.keymap.set('', '<leader>gs', ':Git<CR>')
   -- vim.keymap.set('<silent>', '<leader>gw', ':Gwrite<CR>')
   vim.keymap.set('', '<leader>gc', ':Git commit<CR>')
   vim.keymap.set('', '<leader>gd', ':Gdiffsplit<CR>')
   vim.keymap.set('', '<leader>gp', ':Git pull<CR>')
+  vim.keymap.set('', '<leader>ge', ':Git push<CR>')
   -- SETUP ASYNCRUN // keybind for shell commands
   vim.keymap.set('', '<leader>r', ':AsyncRun -mode=term ')
   vim.keymap.set('', '<leader>e', ':! ')
